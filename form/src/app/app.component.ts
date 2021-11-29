4cf230494194751b3e3da419ab5a1b593eac14f0
unknown <nehalonkar27@gmail.com>
2021-11-29 12:59:51 +0100

    learning NgFor directive

---
+++ form/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'form';    
+}
+class NgForExampleComponent {
+  languages=[
+    {
+      "language": "HTML"
+    },
+    {
+      "language": "CSS"
+    },
+    {
+      "language": "JavaScript"
+    },
+    {
+      "language": "PHP"
+    },
+    {
+      "language": "None"
+    }
+  ];
+}
+
